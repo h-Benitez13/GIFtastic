ef1b70e461a7a0385cbdeb34e865274acb412ab5
h-Benitez13 <hbenitez3711@gmail.com>
2018-12-12 19:46:57 -0500

    pull from the gif with api_key

--- Assets/javascript/gif.js
+++ Assets/javascript/gif.js
@@ -12,14 +12,14 @@ var topics = ["nemo", "bunny", "dog", "bald eagle"];
 function renderButtons() {
     $('.button-group').empty();
 
-    for (var i=0; i < topics.length; i++) {
-      
-       var a = $("<button>");
-       a.addClass("animal");
-       a.attr("data-name", topics[i]);
-       a.text(topics[i]);
-       $(".button-group").append(a);
-      
+    for (var i = 0; i < topics.length; i++) {
+
+        var a = $("<button>");
+        a.addClass("animal");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $(".button-group").append(a);
+
 
     }
     // Delete the content inside the button-group div prior to adding new button
@@ -27,10 +27,10 @@ function renderButtons() {
 
     // Loop through the array of topics, then generate buttons for each topic in the array
 
-  }renderButtons();
+} renderButtons();
 
-  // This function handles events where the add  button is clicked
-  $("#add-animal").on("click", function(event) {
+// This function handles events where the add  button is clicked
+$("#add-animal").on("click", function (event) {
     // event.preventDefault() prevents submit button from trying to send a form.
     // Using a submit button instead of a regular button allows the user to hit
     // "Enter" instead of clicking the button if desired
@@ -44,9 +44,20 @@ function renderButtons() {
 
     // The renderButtons function is called, rendering the list of animals buttons
     renderButtons();
-  });
+});
 
-  // Calling the renderButtons function to display the initial list of animals
-  renderButtons();
+// Calling the renderButtons function to display the initial list of animals
+renderButtons();
+
+$('#add-animal').click('.animal', function(){
+    var topic = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=XDm1iNZRm06xYQ5dvkNgRfGtXJ6kiIHF&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+})
 
 //   next we want to be able to pull data from the gif link 
